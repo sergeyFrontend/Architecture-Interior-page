854d97270a2d6269f449e3455faf0568ac5e60d2
sergeyFrontend <utemishevsergey02@gmail.com>
2023-03-24 00:30:26 +0500

    finished dekstop

--- app/js/main.js
+++ app/js/main.js
@@ -1,6 +1,7 @@
-$(function () {
-    $('.tools__item').accordion({
+const postItems = document.querySelectorAll(".posts__item");
 
-    });
-  
+postItems.forEach((item) => {
+  item.addEventListener("mouseover", () => {
+    item.classList.add("posts__item--active");
+  });
 });
